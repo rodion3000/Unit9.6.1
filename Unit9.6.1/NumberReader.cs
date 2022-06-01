84a9bf02b407e8f5e3282d6b64e0cd2e1fed522c
rodion3000 <rodion.papizh@mail.ru>
2022-06-01 15:25:47 +0500

    Добавьте файлы проекта.

---
+++ Unit9.6.1/NumberReader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit9._6._1
+{
+    internal class NumberReader
+    {
+        public delegate void NumberDelegate(int number);
+        public event NumberDelegate NumberEvent;
+      
+        public void Read()
+        {
+            
+            string[] lastnameMassive = new string[] {"Конников","Пукичев","Владимирович","Рамзанов","Сурэнович"};
+            while (true)
+            {
+                Console.WriteLine("Введите цифру 1 либо 2");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number != 1 && number != 2)
+                {
+                    throw new FormatException();
+                }
+                NumberEntered(number);
+                if (number == 1)
+                {
+                    Array.Sort(lastnameMassive);
+                    foreach (string s in lastnameMassive)
+                    {
+                        Console.WriteLine(s);
+                    }
+
+
+                }
+                if (number == 2)
+                {
+                    Array.Reverse(lastnameMassive);
+                    foreach (string s in lastnameMassive)
+                    {
+                        Console.WriteLine(s);
+                    }
+
+                }
+            }
+
+            
+            
+        }
+        protected virtual void NumberEntered(int number)
+        {
+            NumberEvent?.Invoke(number);
+        }
+        
+    }
+}
---
+++ Unit9.6.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Unit9._6._1
+{
+     class Program
+    {
+        static void Main(string[]args)
+        {
+            NumberReader numberReader = new NumberReader();
+            numberReader.NumberEvent += ShowNumber;
+            try 
+            {
+            numberReader.Read();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено неверно");
+
+            }
+        }
+        static void ShowNumber(int number)
+        {
+            switch (number)
+            {
+                case 1: Console.WriteLine("Введена цифра 1 "); break;
+                case 2: Console.WriteLine("Введена цифра 2"); break;
+
+            }
+
+        }
+    }
+}
+
